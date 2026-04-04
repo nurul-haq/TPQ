e4b4a8390ed5a866d9fa43ed6a6208888d162e91
nurul-haq <nurulhaq.rw10@gmail.com>
2026-04-05 06:04:45 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'tpq-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response('Anda sedang offline. Pastikan koneksi internet stabil.');
+    })
+  );
+});
