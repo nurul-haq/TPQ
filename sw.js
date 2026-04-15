37653e0cb9b8b53354630406ae8c78e0b8b455c0
nurul-haq <nurulhaq.rw10@gmail.com>
2026-04-15 21:38:21 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Versi dinaikkan menjadi v5.1 untuk memicu update PWA
-const CACHE_VERSION = 'v5.5'; 
+const CACHE_VERSION = 'v6.0'; 
 const CACHE_NAME = 'musholla-cache-' + CACHE_VERSION;
 
 const urlsToCache = [
